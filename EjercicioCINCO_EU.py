d7ddd3fec6803650ef82e823bc84e8b18ab3d383
daancabo97 <danielcaicedo285@gmail.com>
2022-12-07 01:17:09 -0500

    push

--- EjercicioCINCO_EU.py
+++ EjercicioCINCO_EU.py
@@ -1,26 +1,31 @@
+# 5. Escribir un programa que me compare dos numeros y me diga cual es mayor
+
+print("EJEMPLO CON UN DICCIONARIO -- 1 ") 
+print("--------------------------------")
+num1 = int(input("Ingrese el primer numero:"))
+num2 = int(input("Ingrese el segundo numero:"))
+
+numeros = {"Num 1": num1, "Num 2": num2}
+
+if num1 == num2:
+    print("Los dos numeros son iguales")
+elif num1 > num2:
+    print(f'{num1} es mayor que {num2}')
+else:
+    print(f'{num2} es mayor que {num1}')    
+    
 # 5. Escribir un programa que me compare dos numeros y me diga cual es mayor 
 
-# EJEMPLO CON UN DICCIONARIO -- "EL EJERCICIO ASIGNADO POR LA PROFE"
-
+print("\n")
+print("EJEMPLO CON UN DICCIONARIO -- 2 ") 
+print("--------------------------------")
 numeros = {'X': 1265, 'Y': 413}
 
-print(numeros)
-print('--------------------------------')
+print(f'numeros:{numeros}')
 
-mayor = max(numeros.keys(), key=lambda A: numeros[A])
+print("--------------------------------")
+mayor = max(numeros.keys(), key=lambda X: numeros[X])
 
 print(f'El mayor es el numero {mayor}')
 
-
-# MISMO EJEMPLO AHORA UTILIZANDO UNA LISTA  -- "EJERCICIO QUE QUERIA PROBAR CON UNA LISTA"
-
-# numeros = [90, 25]
-# min = max = numeros[0]
-# for numero in numeros:
-#     if numero < min:
-#         min = numero
-#     elif numero > max:
-#         max = numero 
-# print("El numero menor es " + str(min)) 
-# print("El numero mayor es " + str(max))
-
+print("\n")
